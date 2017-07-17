acefab44edff038ff30887ba63b9a0c4dafc513f
corno93 <acor5151@uni.sydney.edu.au>
2017-07-18 09:10:15 +1000

    adding avaliable actions for two wheeled env

--- rl_code/src/examples/two_wheeled.cpp
+++ rl_code/src/examples/two_wheeled.cpp
@@ -14,7 +14,27 @@ two_wheeled::~two_wheeled()
 
 std::vector<bool> two_wheeled::available_actions(char s)
 {
-
+    std::vector<bool> actions(4,false);
+    //available actions are those in the same half the state is
+    if(s<=2)
+    {
+        actions[0] = true;
+        actions[1] = true;
+        actions[2] = true;
+        actions[3] = false;
+        actions[4] = false;
+        actions[5] = false;
+    }
+    else if (s >=3)
+    {
+        actions[0] = false;
+        actions[1] = false;
+        actions[2] = false;
+        actions[3] = true;
+        actions[4] = true;
+        actions[5] = true;
+    }
+    return actions;
 }
 
 char two_wheeled::take_action(char action, char current_state)
@@ -29,6 +49,7 @@ char two_wheeled::next_state(char action, char current_state, std::vector<bool> 
 
 signed short int two_wheeled::get_reward(char next_state)
 {
+    signed short int reward;
 
 }
 
