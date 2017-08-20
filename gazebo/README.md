33f3a84ee7b7f2cb4bc10b5e24ca949425a164f0
corno93 <acor5151@uni.sydney.edu.au>
2017-08-20 17:29:17 +1000

    gazebo - episode counts not matching...still can do algs (hopefully)

---
+++ gazebo/README.md
@@ -0,0 +1,1 @@
+The ROS gazebo sim Robosavvy
---
+++ gazebo/restart_sim/include/restart_sim/restart_sim.h
@@ -0,0 +1,13 @@
+//HEADERFILE
+
+#ifndef RESTART_SIM_H
+#define RESTART_SUM_H
+
+#include <string>
+#include <vector>
+#include <map>
+#include <ros/ros.h>
+#include <ros/callback_queue.h>
+
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
