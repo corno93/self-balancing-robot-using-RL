9184d43518d342702727c8789a705f2f4eee428f
Alex <ascornelio@gmail.com>
2017-09-12 14:06:11 +1000

    feedback now happening yew

--- ros/arduino_feedback/src/arduino_feedback_node.py
+++ ros/arduino_feedback/src/arduino_feedback_node.py
@@ -8,31 +8,33 @@ from arduino_feedback.msg import feedback
 if __name__ == '__main__':
 
     try:
-        ardS = serial.Serial("/dev/ttyACM0", 9600)
+        ardS = serial.Serial("/dev/ttyACM0", 38400)
     except:
         rospy.loginfo("Unable to connect to port ttyACM0")
    
 #    msg = PidData()
-    pub = rospy.Publisher('arduino_feedback', String, queue_size=10)
+    pub = rospy.Publisher('arduino_feedback', feedback, queue_size=10)
     rospy.init_node('arduino_feedback_node', anonymous=True)
     rate = rospy.Rate(10) 
     rpm_data1 = []
     msg = feedback()
+	
     while not rospy.is_shutdown():
+
 	try:
 	  #  read_serial = str(int(ardS.readline(), 16))
-            read_serial = ardS.readline()
+            data_recieved = ardS.readline()
 #            msg.rpm1 = read_serial
-                       
-            msg.rpm1 = str(5)
-	    msg.rpm2 = 1.234
-	    hello_str = "hey"
-            rospy.loginfo(hello_str)
-            pub.publish(hello_str)
+            if data_recieved[:2] == 'R1':
+                msg.rpm1 = float(data_recieved[2:])
+            elif data_recieved[:2] == 'R2':
+                msg.rpm2 = float(data_recieved[2:])
+           
+            rospy.loginfo(data_recieved)
+            pub.publish(msg)
             rate.sleep()
 	except:
 	    rospy.loginfo("Unable to readline from arduino")
-            print rpm_data1
 
 
 
